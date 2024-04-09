bdc031918caf967ffa9c1bdb3e81bb8ada1f24d0
M-Gatti <gattimarcus@outlook.com>
2024-04-08 23:08:37 -0300

    moving login file

--- cypress/support/AuthenticationPage.js
+++ cypress/support/AuthenticationPage.js
@@ -1,14 +1,49 @@
 /// <reference types="Cypress"/>
-export default class AuthenticationPage {
 
+const randomEmail = Math.random().toString(20).substring(2,15)+"@gmail.com"
+export default class AuthenticationPage {
+    
     elements = {
-        textEmail: () =>  cy.get('#email'),
-        textPasswd: () =>  cy.get('#passwd')
+        textEmail: () =>  cy.get('#email'),        
+        textPasswd: () =>  cy.get('#passwd'),
+        btnSubmitLogin: () => cy.get('#SubmitLogin > span'),
+        textEmailCreate: () => cy.get('#email_create'),
+        btnCreateAccout: () => cy.get('#SubmitCreate > span'),
+        genderSelect: () => cy.get('#id_gender1'),
+        textFirstName: () =>  cy.get('#customer_firstname'),
+        textLastName: () => cy.get('#customer_lastname'),
+        selectDay: () => cy.get('#days'),
+        selectMonths: () => cy.get('#months'),
+        selectYears: () => cy.get('#years'),
+        btnRegister: () => cy.get('#submitAccount > span')
 
     };
   
-    fillLogin(email,senha) {
+    fillLogin(email,passwd) {
+
         this.elements.textEmail().type("atum1@teste.com") 
         this.elements.textPasswd().type("teste1")
+        this.elements.btnSubmitLogin().click()  
+    }
+
+    fillCreateAccount(email,firstName,lastName,passwd,day,month,year){
+
+        this.elements.textEmailCreate().type(randomEmail)
+        this.elements.btnCreateAccout().click()
+        this.elements.genderSelect().check()
+        this.elements.textFirstName().type("Marcus")
+        this.elements.textLastName().type("Junior")
+        this.elements.textPasswd().type("teste1")
+        this.elements.selectDay().select('14').should('have.value','14')
+        this.elements.selectMonths().select('10').should('have.value','10')
+        this.elements.selectYears().select('2002').should('have.value','2002')
+        this.elements.btnRegister().click()
+    }
+
+    existentAccount(email){
+
+        this.elements.textEmailCreate().type("atum1@teste.com") 
+        this.elements.btnCreateAccout().click()
+
     }
 }
